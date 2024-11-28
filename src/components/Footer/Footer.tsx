d632e3d9cbc977526231121828b8e7c642fb4bfd
Ricardo Gimenez <ricardo.gimenez096@gmail.com>
2024-11-28 11:04:49 -0300

    feat: Add Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+export const Footer = () => {
+
+  return(
+    <div className="w-10/12 text-sm mx-auto box-border p-2 border-2 bg-blue-200 h-auto flex justify-between">
+      <p className="text-surface-600  font-semibold">E-commerce 2024 - 2025 &copy;</p>
+        <ul className="mx-2 flex gap-10 w-30 ">
+          <li>GitHub</li>
+          <li>Contact</li>
+          <li>LinkedIn</li>
+        </ul>     
+    </div>
+  )
+}
